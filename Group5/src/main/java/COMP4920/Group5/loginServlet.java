9b402f8a52231d5c106c5083c01ca51e8fdb4c9a
Xiaowei Zhou <xiaowei.zhou@student.unsw.edu.au>
2018-10-04 18:51:08 +1000

    dynamic page return

--- Group5/src/main/java/COMP4920/Group5/loginServlet.java
+++ Group5/src/main/java/COMP4920/Group5/loginServlet.java
@@ -1,6 +1,8 @@
 package COMP4920.Group5;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +15,13 @@ public class loginServlet extends HttpServlet {
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		System.out.println(username+" "+password);
-		resp.setContentType("text/html);charset=UTF-8");
-		resp.getWriter().print("Login Success");
+		
+		Date curDate = new Date();
+		SimpleDateFormat ft = new SimpleDateFormat("hh:mm:ss a zzz E dd/MM/yyyy");
+		String curTime = ft.format(curDate);
+		req.setAttribute("time", curTime);
+		req.setAttribute("name", "John Doe");
+		req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
